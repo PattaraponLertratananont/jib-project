85907a0bdbd7651c1f3079da859367e2bd9b5a62
MuyoNz <pattaraponler@gmail.com>
2020-08-06 13:59:44 +0700

    and worker to center app || show worker in list and can filter

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ django-admin startproject <name>
 
 <!-- Run server -->
 python manage.py runserver
+
+__init__.py make python can see folder(group module) is a one module
+
+<!-- Add app -->
+at a center app, add app into INSTALLED_APPS
+
+<!-- Migrate module -->
+python manage.py makemigrations
+
+python manage.py migrate
--- safety/workers/admin.py
+++ safety/workers/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Worker
 
-# Register your models here.
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display=(
+        'first_name',
+        'last_name',
+        'is_available',
+        'primary_phone',
+        'secondary_phone',
+    )
+    list_filter = (
+        'is_available',
+    )
