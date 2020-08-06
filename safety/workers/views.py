3e88443c525fee91f4ee75de330d3b16a2410e3b
MuyoNz <pattaraponler@gmail.com>
2020-08-06 15:40:59 +0700

    add template to worker list view

--- safety/workers/views.py
+++ safety/workers/views.py
@@ -1,11 +1,18 @@
 from django.views import View
 from django.http import HttpResponse
+from django.shortcuts import render
+
 from .models import Worker
+
 # class-base view
 class WorkerListView(View):
     def get(self, req):
         workers = Worker.objects.all()
-        html=''
+        # html=''
+        # for worker in workers:
+        #     html += f'<li>{worker.first_name}</li>'
+        # return HttpResponse(html)
+        worker_list = []
         for worker in workers:
-            html += f'<li>{worker.first_name}</li>'
-        return HttpResponse(html)
+            worker_list.append(worker.first_name)
+        return render(req, 'worker_list.html',{'workers':worker_list})
