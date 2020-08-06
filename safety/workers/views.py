95de7c4423bc90836156f99c2cd6a476dc637520
MuyoNz <pattaraponler@gmail.com>
2020-08-06 17:57:39 +0700

    change import line :soccer:

--- safety/workers/views.py
+++ safety/workers/views.py
@@ -1,9 +1,8 @@
 import json
 
+from rest_framework import serializers, viewsets
 from rest_framework.response import Response
-from rest_framework import serializers
 from rest_framework.views import APIView
-from rest_framework import viewsets
 
 from .models import Worker
 
